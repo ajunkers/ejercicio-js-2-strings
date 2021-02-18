4646895df3813bd273b2c4e8e7e047a618a12fb7
Andrea Vidal <11.andrea.v.p@gmail.com>
2021-02-18 19:53:07 +0100

    first commit

---
+++ punto1.js
@@ -0,0 +1,5 @@
+let snakecase = "hello_world";
+
+const resultado1 = snakecase.slice(0, 5) + snakecase.charAt(6).toUpperCase() + snakecase.slice(7);
+
+console.log(resultado1);
---
+++ punto2.js
@@ -0,0 +1,5 @@
+let camelCase = "helloWorld";
+
+const resultado2 = camelCase.slice(0, 5) + "_" + camelCase.slice(5, camelCase.length);
+
+console.log(resultado2);
